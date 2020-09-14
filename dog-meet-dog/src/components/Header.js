f74ffbe541694771060a9f34600cb8dcb83966ab
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-09-14 15:10:27 -0400

    created header component and header css, added navigation icons and styled icons

--- dog-meet-dog/src/components/Header.js
+++ dog-meet-dog/src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "../Header.css"
+import PetsIcon from '@material-ui/icons/Pets';
+import ChatIcon from '@material-ui/icons/Chat';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
 
 function Header() {
     return (
-        <div>
-            <h2>header</h2>
+        <div className="header">
+            <PetsIcon fontSize="large"/>
+            <div className="logo">
+            <LoyaltyIcon fontSize="large" />
+            </div>
+            <ChatIcon fontSize="large"/>
         </div>
     )
 }
