c3338c5878b823196a6c6ec0d12000d91f9f05fc
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-09-14 13:31:29 -0400

    created react app, cleaned up starter code, created header component

---
+++ dog-meet-dog/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dog Meet Dog</h1>
+      <Header/>
+      {/* pet profile cards  */}
+      {/* buttons below profile cards  */}
+
+      {/* messaging screen */}
+      {/* individual messaging screen */}
+
+    </div>
+  );
+}
+
+export default App;
