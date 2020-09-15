09598a0c689b566897393ee9cd1ab18baf94c98a
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-09-15 17:31:58 -0400

    styled messages component

--- dog-meet-dog/src/components/Message.js
+++ dog-meet-dog/src/components/Message.js
@@ -1,15 +1,16 @@
 import React from "react"
+import "../Messages.css"
 import Avatar from "@material-ui/core/Avatar"
 
 function Message({ name, message, timestamp, picture }) {
     return(
         <div className="message">
             <Avatar className="message-image" alt={name} src={picture} />
-            <div>
+            <div className="message-details">
                 <h3>{ name }</h3>
                 <p>{ message }</p>
             </div>
-            <p>{ timestamp }</p>
+            <p className="timestamp">{ timestamp }</p>
         </div>
     )
 }
