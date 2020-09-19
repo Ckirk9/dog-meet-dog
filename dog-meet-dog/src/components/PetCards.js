9ee6bd10cff03640ac0a5b11bece01e481548d29
Ckirk9 <66570605+Ckirk9@users.noreply.github.com>
2020-09-19 14:24:56 -0400

    hooked up to prod db to see actual profiles versus the hardcoded profiles, updated useeffect method, bio now showing on pet card styling wip

--- dog-meet-dog/src/components/PetCards.js
+++ dog-meet-dog/src/components/PetCards.js
@@ -1,23 +1,26 @@
 import React, {useState, useEffect} from "react"
 import TinderCard from "react-tinder-card"
-import database from "../firebase"
+import PetModel from "../models/pet"
 import "../PetCards.css"
 
 const PetCards= () => {
     const [pets, setPets] = useState([]);
+    const [currentPet] = useState(localStorage.getItem('uid'))
 
+    // Nesting async function after receiving a propmt from the hooks warning to do so
     useEffect(() => {
-        // runs through the pets collection in the DB and anytime a new pet is added compent will re-render
-        //when does useEffect get triggered?
-        database.collection("pets").onSnapshot((snapshot) =>
-              setPets(snapshot.docs.map((doc) => doc.data())
-            ))
-    }, [])
+        const fetchPets = async () => {
+            //when does useEffect get triggered?
+            const petsResponse = await PetModel.all(currentPet);
+            setPets(petsResponse);
+        }
+        fetchPets();
+    }, [currentPet])
 
     return(
         <div className="pet-card">
             {/* conditional here so that you don't match with yourself */}
-            {pets.map(pet => (
+            { pets.pets ? pets.pets.map(pet => (
                 // courtesy of react-tinder-card already includes swipe functionality
                 <TinderCard
                     className="swipe"
@@ -25,12 +28,13 @@ const PetCards= () => {
                     preventSwipe={['up', 'down']}
                 >
                     <div 
-                        style={{ backgroundImage: `url(${pet.url})`}}
+                        style={{ backgroundImage: `url(${pet.pictureUrl})`}}
                         className="card">
                         <h4>{pet.username}</h4>
+                        <h4>{pet.bio}</h4>
                     </div>
                 </TinderCard>
-            ))}
+            )) : null}
         </div>
     )
 }
--- dog-meet-dog/src/models/pet.js
+++ dog-meet-dog/src/models/pet.js
@@ -1,11 +1,34 @@
 const url = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/v1'
 
 export default class PetModel {
+    // we are requiring the username here to later ensure we exclude "liking" our own profile
+    static async all(username) {
+        const res = await fetch(`${url}/pets/${username}`)
+        return await res.json()
+    }
+
+    static async show(username){
+        //here we are using pet instead of pets so we can view one profile specifically either our own or one of our matches
+        const res = await fetch(`${url}/pet/${username}`)
+        return await res.json()
+    }
+
     static async create(data) {
         const res = await fetch(`${url}/auth/signUp`, {
             method: "POST",
             headers: { "Content-Type": "application/json"},
             body: JSON.stringify(data)
+        })
+        return await res.json()
+    }
+
+    static async edit(pet) {
+        const res = await fetch(`${url}/pets/${pet.username}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(pet)
         })
         return await res.json()
     }
@@ -16,6 +39,13 @@ export default class PetModel {
             headers: { "Content-Type": "application/json" },
             credentials: "include",
             body: JSON.stringify(credentials)
+        })
+        return await res.json()
+    }
+
+    static async destroy(id) {
+        const res = await fetch(`${url}/pets/${id}`, {
+            method: "DELETE"
         })
         return await res.json()
     }
